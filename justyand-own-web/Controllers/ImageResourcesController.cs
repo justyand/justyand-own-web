9fc8b5aff0c66ce59271dbd20d36298fcbeefc43
justyand <justyand@gmail.com>
2016-12-23 11:03:03 +0100

    Add project files.

---
+++ justyand-own-web/Controllers/ImageResourcesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Resources;
+using System.Web;
+using System.Web.Mvc;
+
+namespace justyand_own_web.Controllers
+{
+    public class ImageResourcesController : Controller
+    {
+        // GET: ImageResources
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public FileStreamResult Image(string key)
+        {
+            var bitmap = justyand_own_web.Properties.Resources.logo;
+            MemoryStream stream = new MemoryStream();
+            bitmap.Save(stream, ImageFormat.Png);
+            stream.Seek(0, SeekOrigin.Begin);
+            return new FileStreamResult(stream, "image/png");
+        }
+    }
+}
---
+++ justyand-own-web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="justyand_own_web.Global" Language="C#" %>
